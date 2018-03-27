bb9bfbb45bea43d559a67bd53250f554d921b051
danielvportlandia <danielshelton90@gmail.com>
2018-03-27 10:46:46 -0700

    out.sql file added.

--- server.js
+++ server.js
@@ -18,11 +18,19 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.log(err));
 
+// application middleware
+app.use(cors());
+
 //API endpoints
 
-app.get('/api/v1/books', (req, res) => res.send('It lives!'));
+app.get('/api/v1/books', (req, res) => {
+    client.query('SELECT book_id, title, author, image_url, isbn FROM books;')
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
 //This app.get will need a lot more fleshing out once the database is operational.
 
 // app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+
