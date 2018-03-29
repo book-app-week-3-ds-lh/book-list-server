17dac7e81e50ce8abf00f4b4bdec8e0a6e2e97d7
LACY HOGAN <lacy.hogan@gmail.com>
2018-03-28 17:32:47 -0700

    Added form PUT

--- server.js
+++ server.js
@@ -57,7 +57,25 @@ app.post('/api/v1/books', (req, res) => {
     });
 });
 
+app.put('api/v1/books', (req, res) => {
+  client.query(`
+  UPDATE books
+  SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5
+  WHERE book_id=$6
+  `, [
+    req.body.title,
+    req.body.author,
+    req.body.isbn,
+    req.body.img_url,
+    req.body.description,
+    req.params.id
+  ])
+    .then(() => res.send('Update complete'))
+    .catch(function(err) {
+      console.error(err);
+    });
+});
+
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
-
